7c7e5cedd454cd9796e64f6ba660fba2c789eccb
Sean Dawson <contact@seandawson.info>
2019-04-21 21:44:14 +1000

    Update some project dependencies

--- project.clj
+++ project.clj
@@ -3,18 +3,19 @@
   :url "http://github.com/noxharmonium/nanoweave"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.blancas/kern "1.1.0"]
-                 [org.clojure/tools.cli "0.3.7"]
+                 [org.clojure/tools.cli "0.4.2"]
                  [prismatic/schema "1.1.9"]
                  [org.clojure/data.json "0.2.6"]
-                 [rhizome "0.2.9"]]
+                 [rhizome "0.2.9"]
+                 [org.clojure/tools.namespace "0.2.7"]]
   :main ^:skip-aot nanoweave.core
   :target-path "target/%s"
   :jvm-opts ["-Djava.awt.headless=true"]
-  :plugins [[lein-codox "0.10.4"]]
-  :profiles {
-             :uberjar {:aot :all}
-             :dev {:resource-paths ["test/resources"]}
-             }
-  )
+  :plugins [[lein-codox "0.10.4"]
+            [cider/cider-nrepl "0.21.1"]]
+  :profiles {:uberjar {:aot :all}
+             :dev {:resource-paths ["test/resources"]}})
+
+
