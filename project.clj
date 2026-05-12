b0b8503c4e9b8d6dcaa97430af4a081af044676f
Sean Dawson <contact@seandawson.info>
2026-05-12 20:55:04 +1000

    build: move to github actions and publish demo to GH pages

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nanoweave "1.2.8"
+(defproject nanoweave "1.2.8" ; x-release-please-version
   :description "A data transformation tool"
   :url "http://github.com/noxharmonium/nanoweave"
   :license {:name "Apache License, Version 2.0"
